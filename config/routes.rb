977c9071799ab37f33653a15063b76516f375641
fukah1re <fukahire1216@gmail.com>
2020-09-04 23:18:12 +0900

    Feature/#2 create rough view of the sign-up screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'posts#index'
   get 'posts/index'
   get 'posts/show'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_in", to: "users/sessions#destroy"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
