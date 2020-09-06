7b73e85234c458dd606e7d16e6d9936bd0e732c2
fukah1re <fukahire1216@gmail.com>
2020-09-06 22:50:30 +0900

    feature/#12 ユーザモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,36 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable
+
+  has_one_attached :profile_image
+
+  has_many :pets, dependent: :destroy
+  has_many :my_posts, through: :pets, source: :posts
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_posts, through: :favorites, source: :posts
+  has_many :comments, dependent: :destroy
+
+  validates :username, presence: true
+  validates :introduce, length: { maximum: 150 }
+
+  # 自分がフォローしているユーザとの関連
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followings, through: :active_relationships, source: :followed
+
+  # 自分をフォローしているユーザとの関連
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # フォロー済みかチェック
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_025440) do
+ActiveRecord::Schema.define(version: 2020_09_05_133324) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_09_05_025440) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.text "introduce"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
